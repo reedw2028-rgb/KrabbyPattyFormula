003c7b2bce63c9f5ec23243a2ae5ed9f6a1327a1
reedw2028-rgb <231079625+reedw2028-rgb@users.noreply.github.com>
2025-12-02 13:47:36 -0800

    Add files via upload

---
+++ krabby.js
@@ -0,0 +1,12 @@
+function fatboat() {
+    document.getElementById("milkshake").innerHTML = "You clicked the button! That means you love krabby patty's!❤️  ";
+}
+
+function myFunction() {
+    let X = document.getElementById("jacobo").value ;
+    document.getElementById("edward").innerHTML = "Here is your reveiw: " + X;
+}
+
+function dropdown() {
+  document.getElementById("beanz").value ;
+}
